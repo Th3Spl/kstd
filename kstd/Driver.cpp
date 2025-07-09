a9c3c338132d1ce014b8decbe298b5ab94fc90d6
Th3Spl <86083815+Th3Spl@users.noreply.github.com>
2025-07-09 19:03:19 +0200

    Update Driver.cpp
    added tests
    
    Co-Authored-By: FURYWOLF <123304011+xxfurywolfxx@users.noreply.github.com>

--- kstd/Driver.cpp
+++ kstd/Driver.cpp
@@ -21,30 +21,28 @@ NTSTATUS DriverEntry( PDRIVER_OBJECT drv_obj, PUNICODE_STRING reg_path )
 	NTSTATUS status = STATUS_SUCCESS;
 	kstd::string my_string = "abcdefghijklmnopqrstuvwxyz";
 	kstd::wstring my_wstring = L"abcdefghijklmnopqrstuvwxyz";
-	//kstd::initializer_list<ULONG> x(1, 2);
 	vector<int> my_vector = { 1, 2 };
 
 
 	/* string */
-	//DbgPrintEx( 0, 0, "(~) IsEmpty: %s", my_string.empty( ) ? "IsEmpty" : "NotEmpty" );
-	//DbgPrintEx( 0, 0, "(~) Test  : %s", my_string.c_str( ) );
-	//DbgPrintEx( 0, 0, "(~) Test2 : %c", my_string[2] );
-	//DbgPrintEx( 0, 0, "(~) Test3 : %c", *(char*)my_string.end() );
-	//DbgPrintEx( 0, 0, "(~) Test4 : %s", my_string.nofree_substr(1, my_string.size() - 2) );
-	//DbgPrintEx( 0, 0, "\n" );
+	DbgPrintEx( 0, 0, "(~) IsEmpty: %s", my_string.empty( ) ? "IsEmpty" : "NotEmpty" );
+	DbgPrintEx( 0, 0, "(~) Test  : %s", my_string.c_str( ) );
+	DbgPrintEx( 0, 0, "(~) Test2 : %c", my_string[2] );
+	DbgPrintEx( 0, 0, "(~) Test3 : %c", *(char*)my_string.end() );
+	DbgPrintEx( 0, 0, "(~) Test4 : %s", my_string.nofree_substr(1, my_string.size() - 2) );
+	DbgPrintEx( 0, 0, "\n" );
+
 
 	/* wstring */
-	//UNICODE_STRING nigger;
-	//my_wstring.to_unicode( &nigger );
-	//DbgPrintEx( 0, 0, "(~) wstring now:" );
-	//DbgPrintEx( 0, 0, "(~) IsEmpty : %s", my_wstring.empty( ) ? "IsEmpty" : "NotEmpty" );
-	//DbgPrintEx( 0, 0, "(~) Test    : %ws", my_wstring.c_str( ) );
-	//DbgPrintEx( 0, 0, "(~) Test2   : %wc", my_wstring[ 2 ] );
-	//DbgPrintEx( 0, 0, "(~) Test3   : %wc", *( char* )my_wstring.end( ) );
-	//DbgPrintEx( 0, 0, "(~) Test4   : %ws", my_wstring.nofree_substr( 1, my_wstring.size( ) - 2 ) );
-	//DbgPrintEx( 0, 0, "(~) Test5   : %wZ", nigger );
-
-
+	UNICODE_STRING nigger;
+	my_wstring.to_unicode( &nigger );
+	DbgPrintEx( 0, 0, "(~) wstring now:" );
+	DbgPrintEx( 0, 0, "(~) IsEmpty : %s", my_wstring.empty( ) ? "IsEmpty" : "NotEmpty" );
+	DbgPrintEx( 0, 0, "(~) Test    : %ws", my_wstring.c_str( ) );
+	DbgPrintEx( 0, 0, "(~) Test2   : %wc", my_wstring[ 2 ] );
+	DbgPrintEx( 0, 0, "(~) Test3   : %wc", *( char* )my_wstring.end( ) );
+	DbgPrintEx( 0, 0, "(~) Test4   : %ws", my_wstring.nofree_substr( 1, my_wstring.size( ) - 2 ) );
+	DbgPrintEx( 0, 0, "(~) Test5   : %wZ", nigger );
 
 
 	/* vector */
