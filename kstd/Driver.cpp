1adaea44d790ad28a6f787a1108b0ca9b2b476d9
FURYWOLF <123304011+xxFURYWOLFxx@users.noreply.github.com>
2025-08-08 15:55:29 +0300

    vector 1.1

--- kstd/Driver.cpp
+++ kstd/Driver.cpp
@@ -1,4 +1,4 @@
-
+﻿
 //
 // inclusions
 //
@@ -20,7 +20,9 @@ NTSTATUS DriverEntry( PDRIVER_OBJECT drv_obj, PUNICODE_STRING reg_path )
 	NTSTATUS status = STATUS_SUCCESS;
 	kstd::string my_string = "abcdefghijklmnopqrstuvwxyz";
 	kstd::wstring my_wstring = L"abcdefghijklmnopqrstuvwxyz";
-	kstd::vector<int> my_vector = { 1, 2 };
+
+    kstd::vector<int> my_vector ; // must innitialize before usage.
+
 	UNICODE_STRING my_unicode;
 
 
@@ -44,11 +46,74 @@ NTSTATUS DriverEntry( PDRIVER_OBJECT drv_obj, PUNICODE_STRING reg_path )
 	DbgPrintEx( 0, 0, "(~) Test5   : %wZ", my_unicode );
 
 
-	/* vector */
-	for ( int i = 0; i < 10; i++ )
-	{
-		my_vector.push_back( i );
-	}
+
+    /* vector testing */
+    DbgPrintEx(0, 0, "(~) vector now:\n");
+    DbgPrintEx(0, 0, "(~) IsEmpty : %s\n", my_vector.empty() ? "IsEmpty" : "NotEmpty");
+    DbgPrintEx(0, 0, "(~) Length  : %d\n", my_vector.len());
+    DbgPrintEx(0, 0, "(~) Size    : %d\n", my_vector.raw_size());
+    DbgPrintEx(0, 0, "(~) Capacity: %d\n", my_vector.raw_capacity());
+    DbgPrintEx(0, 0, "(~) Front   : %d\n", my_vector.front());
+    DbgPrintEx(0, 0, "(~) Back    : %d\n", my_vector.back());
+    DbgPrintEx(0, 0, "(~) At(0)   : %d\n", my_vector.at(0));
+    DbgPrintEx(0, 0, "(~) [1]     : %d\n", my_vector[1]);
+    DbgPrintEx(0, 0, "\n");
+
+    /* push_back test */
+    my_vector.push_back(3);
+    my_vector.push_back(4);
+    my_vector.push_back(5);
+    DbgPrintEx(0, 0, "(~) After push_back(3,4,5):\n");
+    for (ULONG i = 0; i < my_vector.len(); i++)
+    {
+        DbgPrintEx(0, 0, "(~) [%d]     : %d\n", i, my_vector[i]);
+    }
+    DbgPrintEx(0, 0, "\n");
+
+    /* pop_back test */
+    int popped = my_vector.pop_back();
+    DbgPrintEx(0, 0, "(~) Popped  : %d\n", popped);
+    DbgPrintEx(0, 0, "(~) NewLen  : %d\n", my_vector.len());
+    DbgPrintEx(0, 0, "\n");
+
+    /* insert test */
+    my_vector.insert(2, 99);
+    DbgPrintEx(0, 0, "(~) After insert(2, 99):\n");
+    for (ULONG i = 0; i < my_vector.len(); i++)
+    {
+        DbgPrintEx(0, 0, "(~) [%d]     : %d\n", i, my_vector[i]);
+    }
+    DbgPrintEx(0, 0, "\n");
+
+    /* erase test */
+    my_vector.erase(2);
+    DbgPrintEx(0, 0, "(~) After erase(2):\n");
+    for (ULONG i = 0; i < my_vector.len(); i++)
+    {
+        DbgPrintEx(0, 0, "(~) [%d]     : %d\n", i, my_vector[i]);
+    }
+    DbgPrintEx(0, 0, "\n");
+
+    /* find test */
+    ULONG pos = my_vector.find(3);
+    DbgPrintEx(0, 0, "(~) Find(3) : %d\n", pos);
+    DbgPrintEx(0, 0, "(~) Contains(3): %s\n", my_vector.contains(3) ? "True" : "False");
+    DbgPrintEx(0, 0, "(~) Contains(99): %s\n", my_vector.contains(99) ? "True" : "False");
+    DbgPrintEx(0, 0, "\n");
+
+    /* resize test */
+    my_vector.resize(10);
+    DbgPrintEx(0, 0, "(~) After resize(10):\n");
+    DbgPrintEx(0, 0, "(~) Length  : %d\n", my_vector.len());
+    DbgPrintEx(0, 0, "\n");
+
+    /* clear test */
+    my_vector.clear();
+    DbgPrintEx(0, 0, "(~) After clear:\n");
+    DbgPrintEx(0, 0, "(~) IsEmpty : %s\n", my_vector.empty() ? "IsEmpty" : "NotEmpty");
+    DbgPrintEx(0, 0, "(~) Length  : %d\n", my_vector.len());
+
+    my_vector.cleanup();
 
 	return status;
 }
--- kstd/kstd/vector.hpp
+++ kstd/kstd/vector.hpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 
 	The scope of this file is to define the backbone structure
 	implementation in kernel mode of std::vector<> ( with a lil less functionalities )
@@ -33,137 +33,503 @@ inline namespace kstd {
 	public:
 
 		/* empty constructor */
-		inline __fastcall vector( ) { this->initialized = true; };
-
-		/* value initializer for { 1, 2 } constructor*/
-		void __init_values__( size_t index ) { index; }
-		template<typename T, typename... Params>
-		void __init_values__( size_t index, T&& first, Params&&... rest )
+		inline __fastcall vector()
 		{
-			this->buffer[index] = first;
-			__init_values__( index + 1, rest... );
+			this->buffer = nullptr;
+			this->size = 0;
+			this->length = 0;
+			this->capacity = 0;
+			this->initialized = false;
 		}
 
-		/* { 1, 2 } constructor */
-		template<typename... params>
-		inline __fastcall vector( params... init_params )
+		/* variadic constructor for { 1, 2 } syntax */
+		template<typename... Args>
+		inline __fastcall vector(Args... args)
 		{
-			/* getting the current size */
-			this->length = sizeof...( init_params );
-			this->size = this->length * sizeof( T );
+			this->buffer = nullptr;
+			this->size = 0;
+			this->length = 0;
+			this->capacity = 0;
+			this->initialized = false;
+
+			if (!__ensure_initialized__()) return;
+
+			this->length = sizeof...(args);
+			this->size = this->length * sizeof(T);
 			this->capacity = this->size * 2;
 
-			/* allocating the needed memory */
-			this->buffer = ( T* )ExAllocatePool2( POOL_FLAG_NON_PAGED, this->capacity, ( ULONG )__rdtsc( ) );
-			if ( !this->buffer ) return;
+			if (this->capacity < 64) this->capacity = 64;
 
-			/* initializing the vector with the provided values */
-			__init_values__( 0, init_params... );
+			this->buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->capacity, (ULONG)__rdtsc());
+			if (!this->buffer)
+			{
+				this->length = 0;
+				this->size = 0;
+				this->capacity = 0;
+				return;
+			}
 
+			__init_values__(0, args...);
 			this->initialized = true;
-			return;
+		}
+
+		/* size constructor */
+		inline __fastcall vector(ULONG count)
+		{
+			this->length = count;
+			this->size = this->length * sizeof(T);
+			this->capacity = this->size * 2;
+
+			this->buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->capacity, (ULONG)__rdtsc());
+			if (!this->buffer) return;
+
+			memset(this->buffer, 0, this->size);
+			this->initialized = true;
+		}
+
+		/* copy constructor */
+		inline __fastcall vector(const vector<T>& other)
+		{
+			this->length = other.length;
+			this->size = other.size;
+			this->capacity = other.capacity;
+
+			this->buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->capacity, (ULONG)__rdtsc());
+			if (!this->buffer) return;
+
+			memcpy(this->buffer, other.buffer, this->size);
+			this->initialized = true;
+		}
+
+		/* assignment operator */
+		inline vector<T>& operator=(const vector<T>& other)
+		{
+			if (this != &other)
+			{
+				__try
+				{
+					if (this->buffer)
+						ExFreePoolWithTag(this->buffer, 0);
+
+					this->length = other.length;
+					this->size = other.size;
+					this->capacity = other.capacity;
+
+					if (other.buffer && other.capacity > 0)
+					{
+						this->buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->capacity, (ULONG)__rdtsc());
+						if (this->buffer)
+						{
+							memcpy(this->buffer, other.buffer, this->size);
+							this->initialized = true;
+						}
+						else
+						{
+							this->length = 0;
+							this->size = 0;
+							this->capacity = 0;
+							this->initialized = false;
+						}
+					}
+					else
+					{
+						this->buffer = nullptr;
+						this->initialized = other.initialized;
+					}
+				}
+				__except (EXCEPTION_EXECUTE_HANDLER)
+				{
+					this->buffer = nullptr;
+					this->length = 0;
+					this->size = 0;
+					this->capacity = 0;
+					this->initialized = false;
+				}
+			}
+			return *this;
+		}
+
+		/* assignment from multiple values */
+		template<typename... Args>
+		inline vector<T>& assign(Args... args)
+		{
+			__try
+			{
+				if (this->buffer)
+					ExFreePoolWithTag(this->buffer, 0);
+
+				this->length = sizeof...(args);
+				this->size = this->length * sizeof(T);
+				this->capacity = this->size * 2;
+
+				if (this->capacity < 64) this->capacity = 64;
+
+				this->buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->capacity, (ULONG)__rdtsc());
+				if (!this->buffer)
+				{
+					this->length = 0;
+					this->size = 0;
+					this->capacity = 0;
+					this->initialized = false;
+					return *this;
+				}
+
+				__init_values__(0, args...);
+				this->initialized = true;
+			}
+			__except (EXCEPTION_EXECUTE_HANDLER)
+			{
+				this->buffer = nullptr;
+				this->length = 0;
+				this->size = 0;
+				this->capacity = 0;
+				this->initialized = false;
+			}
+
+			return *this;
 		}
 
 		/* push back ( adding an item ) */
-		inline void push_back( T item )
+		inline bool push_back(T item)
 		{
-			/* rellocating the buffer if needed */
-			if ( this->size + sizeof( T ) > this->capacity )
-				this->__realloc__( );
+			if (!__ensure_initialized__()) return false;
 
-			/* adding the item */
-			if ( this->size + sizeof( T ) <= this->capacity )
+			if (this->size + sizeof(T) > this->capacity)
 			{
-				this->buffer[this->length] = item;
-				this->size += sizeof( T );
-				this->length++;
+				if (!this->__realloc__()) return false;
 			}
 
-			return;
+			this->buffer[this->length] = item;
+			this->size += sizeof(T);
+			this->length++;
+
+			return true;
 		}
 
 		/* pop back ( removing an item ) */
-		inline T pop_back( )
+		inline T pop_back()
 		{
+			if (!this->initialized || this->length == 0) return T{};
+
 			T ret = this->buffer[this->length - 1];
-			memset( this->buffer[this->length - 1], 0, sizeof( T ) );
+			this->length--;
+			this->size -= sizeof(T);
 
 			return ret;
 		}
 
-		/* clear */
-		inline void clear( bool dealloc = false )
+		/* insert at position */
+		inline bool insert(ULONG pos, const T& value)
 		{
-			/* fixing */
+			if (!__ensure_initialized__()) return false;
+			if (pos > this->length) return false;
+
+			if (this->size + sizeof(T) > this->capacity)
+			{
+				if (!this->__realloc__()) return false;
+			}
+
+			for (ULONG i = this->length; i > pos; i--)
+				this->buffer[i] = this->buffer[i - 1];
+
+			this->buffer[pos] = value;
+			this->length++;
+			this->size += sizeof(T);
+
+			return true;
+		}
+
+		/* erase at position */
+		inline bool erase(ULONG pos)
+		{
+			if (!this->initialized || pos >= this->length) return false;
+
+			for (ULONG i = pos; i < this->length - 1; i++)
+				this->buffer[i] = this->buffer[i + 1];
+
+			this->length--;
+			this->size -= sizeof(T);
+
+			return true;
+		}
+
+		/* resize vector */
+		inline bool resize(ULONG new_size)
+		{
+			if (!__ensure_initialized__()) return false;
+
+			if (new_size * sizeof(T) > this->capacity)
+			{
+				ULONG new_capacity = new_size * sizeof(T) * 2;
+				if (new_capacity < 64) new_capacity = 64;
+
+				T* new_buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, new_capacity, (ULONG)__rdtsc());
+				if (!new_buffer) return false;
+
+				if (this->buffer)
+				{
+					memcpy(new_buffer, this->buffer, this->size);
+					ExFreePoolWithTag(this->buffer, 0);
+				}
+				this->buffer = new_buffer;
+				this->capacity = new_capacity;
+			}
+
+			if (new_size > this->length)
+				memset(&this->buffer[this->length], 0, (new_size - this->length) * sizeof(T));
+
+			this->length = new_size;
+			this->size = new_size * sizeof(T);
+
+			return true;
+		}
+
+		/* reserve capacity */
+		inline bool reserve(ULONG new_capacity)
+		{
+			if (!__ensure_initialized__()) return false;
+			if (new_capacity <= this->capacity) return true;
+
+			T* new_buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, new_capacity, (ULONG)__rdtsc());
+			if (!new_buffer) return false;
+
+			if (this->buffer)
+			{
+				memcpy(new_buffer, this->buffer, this->size);
+				ExFreePoolWithTag(this->buffer, 0);
+			}
+			this->buffer = new_buffer;
+			this->capacity = new_capacity;
+
+			return true;
+		}
+
+		/* get front element */
+		inline T& front()
+		{
+			if (!this->initialized || this->length == 0)
+			{
+				static T dummy = T{};
+				return dummy;
+			}
+			return this->buffer[0];
+		}
+
+		/* get back element */
+		inline T& back()
+		{
+			if (!this->initialized || this->length == 0)
+			{
+				static T dummy = T{};
+				return dummy;
+			}
+			return this->buffer[this->length - 1];
+		}
+
+		/* at with bounds checking */
+		inline T& at(ULONG pos)
+		{
+			if (!this->initialized || pos >= this->length)
+			{
+				static T dummy = T{};
+				return dummy;
+			}
+			return this->buffer[pos];
+		}
+
+		/* find element */
+		inline ULONG find(const T& value)
+		{
+			if (!this->initialized) return 0;
+
+			for (ULONG i = 0; i < this->length; i++)
+			{
+				if (this->buffer[i] == value)
+					return i;
+			}
+			return this->length;
+		}
+
+		/* contains element */
+		inline bool contains(const T& value)
+		{
+			return find(value) != this->length;
+		}
+
+		/* clear */
+		inline void clear(bool dealloc = false)
+		{
+			if (!this->initialized) return;
+
 			this->size = 0;
 			this->length = 0;
 
-			/* action */
-			if ( dealloc ) ExFreePoolWithTag( this->buffer, 0 );
-			else memset( this->buffer, 0, this->capacity );
+			if (dealloc)
+			{
+				if (this->buffer)
+				{
+					ExFreePoolWithTag(this->buffer, 0);
+					this->buffer = nullptr;
+					this->capacity = 0;
+					this->initialized = false;
+				}
+			}
+			else if (this->buffer)
+			{
+				memset(this->buffer, 0, this->capacity);
+			}
+		}
 
-			return;
+		/* shrink to fit */
+		inline bool shrink_to_fit()
+		{
+			if (!this->initialized || this->size == this->capacity || this->size == 0) return true;
+
+			T* new_buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->size, (ULONG)__rdtsc());
+			if (!new_buffer) return false;
+
+			memcpy(new_buffer, this->buffer, this->size);
+			ExFreePoolWithTag(this->buffer, 0);
+			this->buffer = new_buffer;
+			this->capacity = this->size;
+
+			return true;
 		}
 
 		/* get data */
-		inline T* data( ) { return this->buffer; }
+		inline T* data() { return this->buffer; }
 
 		/* get the size ( in items ) */
-		inline ULONG len( ) { return this->length; }
+		inline ULONG len() { return this->length; }
 
 		/* get the size in bytes */
-		inline ULONG raw_size( ) { return this->size; }
+		inline ULONG raw_size() { return this->size; }
 
 		/* get the current capacity */
-		inline ULONG raw_capacity( ) { return this->capacity; }
+		inline ULONG raw_capacity() { return this->capacity; }
 
 		/* check if the vector is empty */
-		inline bool empty( ) { return this->length > 0; }
+		inline bool empty() { return this->length == 0; }
+
+		/* check if vector is properly initialized */
+		inline bool is_valid() { return this->initialized && this->buffer != nullptr; }
 
 		/* operator [] with index */
-		inline T __fastcall operator[]( unsigned long idx )
+		inline T& __fastcall operator[](unsigned long idx)
 		{
+			if (!this->initialized || idx >= this->length)
+			{
+				static T dummy = T{};
+				return dummy;
+			}
 			return this->buffer[idx];
 		}
 
 		/* operator [] const with index */
-		inline const T __fastcall operator[]( unsigned long idx ) const
+		inline const T& __fastcall operator[](unsigned long idx) const
 		{
+			if (!this->initialized || idx >= this->length)
+			{
+				static T dummy = T{};
+				return dummy;
+			}
 			return this->buffer[idx];
 		}
 
-		/* destructor */
-		~vector( ) { ExFreePoolWithTag( this->buffer, 0 ); }
+		/* begin iterator (pointer) */
+		inline T* begin() { return this->buffer; }
+
+		/* end iterator (pointer) */
+		inline T* end() { return this->buffer + this->length; }
+
+		/* manual cleanup - call this instead of relying on destructor */
+		inline void cleanup()
+		{
+			if (this->initialized && this->buffer)
+			{
+				ExFreePoolWithTag(this->buffer, 0);
+				this->buffer = nullptr;
+				this->initialized = false;
+				this->size = 0;
+				this->length = 0;
+				this->capacity = 0;
+			}
+		}
+
+		/* destructor - empty to avoid dynamic initialization */
+		~vector() = default;
 
 	private:
 
-		/* attributes */
-		T* buffer = nullptr;
-		ULONG size = 0;
-		ULONG length = 0;
-		ULONG capacity = 0;
-		bool initialized = false;
+		/* value initializer helper */
+		void __init_values__(size_t index) { index; }
+		template<typename U, typename... Params>
+		void __init_values__(size_t index, U&& first, Params&&... rest)
+		{
+			this->buffer[index] = first;
+			__init_values__(index + 1, rest...);
+		}
+
+		/* auto-initialize check - handles uninitialized global vectors */
+		inline bool __ensure_initialized__()
+		{
+			if (this->initialized != true && this->initialized != false)
+			{
+				__try
+				{
+					this->buffer = nullptr;
+					this->size = 0;
+					this->length = 0;
+					this->capacity = 0;
+					this->initialized = false;
+				}
+				__except (EXCEPTION_EXECUTE_HANDLER)
+				{
+					return false;
+				}
+			}
+
+			if (!this->initialized)
+			{
+				this->capacity = 64;
+				this->buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, this->capacity, (ULONG)__rdtsc());
+				if (!this->buffer)
+				{
+					this->capacity = 0;
+					return false;
+				}
+				this->initialized = true;
+			}
+
+			return true;
+		}
 
 		/* realloc --> copy */
-		inline void __realloc__( )
+		inline bool __realloc__()
 		{
-			/* calculating the new capacity */
-			ULONG new_capacity = 2 * ( this->capacity + sizeof( T ) );
+			ULONG new_capacity = this->capacity == 0 ? 64 : 2 * this->capacity;
 
-			/* allocating the new buffer */
-			T* new_buffer = ( T* )ExAllocatePool2( POOL_FLAG_NON_PAGED, new_capacity, ( ULONG )__rdtsc( ) );
-			if ( !new_buffer ) return;
+			T* new_buffer = (T*)ExAllocatePool2(POOL_FLAG_NON_PAGED, new_capacity, (ULONG)__rdtsc());
+			if (!new_buffer) return false;
 
-			/* copying the mem */
-			memcpy( new_buffer, this->buffer, this->size );
+			if (this->buffer && this->size > 0)
+			{
+				memcpy(new_buffer, this->buffer, this->size);
+				ExFreePoolWithTag(this->buffer, 0);
+			}
 
-			/* swapping the buffer */
-			ExFreePoolWithTag( this->buffer, 0 );
 			this->buffer = new_buffer;
 			this->capacity = new_capacity;
 
-			return;
+			return true;
 		}
+
+		/* attributes */
+		T* buffer;
+		ULONG size;
+		ULONG length;
+		ULONG capacity;
+		bool initialized;
 
 	};
 }
